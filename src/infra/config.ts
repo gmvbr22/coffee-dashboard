86219cc9e1ed2ca0cc23b09d883ed0196f5dc2f4
gmvbr <87571591+gmvbr@users.noreply.github.com>
2021-12-09 20:11:44 -0400

    Create Config

---
+++ src/infra/config.ts
@@ -0,0 +1,26 @@
+import {Service} from 'typedi';
+
+import S from 'fluent-json-schema';
+import envSchema from 'env-schema';
+
+export interface Env {
+  port: number;
+  host: string;
+}
+
+@Service()
+export class Config {
+  public env?: Env;
+
+  /**
+   * Carrega as variáveis de ambiente
+   */
+  public loadEnv() {
+    this.env = envSchema({
+      dotenv: true,
+      schema: S.object()
+        .prop('port', S.string().default('3000').required())
+        .prop('host', S.string().default('0.0.0.0').required()),
+    });
+  }
+}
