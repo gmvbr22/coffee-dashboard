37ccc5a354a963313b96a28b573f43cc327494fe
gmvbr <87571591+gmvbr@users.noreply.github.com>
2021-12-09 20:47:34 -0400

    Create User

---
+++ src/domain/user.ts
@@ -0,0 +1,29 @@
+/**
+ * Classe que representa a entidade usuário
+ */
+export class User {
+  /**
+   * ID do usuário
+   */
+  public id?: string;
+
+  /**
+   * Nome do usuário
+   */
+  public name?: string;
+
+  /**
+   * Email do usuário
+   */
+  public email?: string;
+
+  /**
+   * Senha do usuário
+   */
+  public password?: string;
+
+  /**
+   * Cargo do usuário
+   */
+  public role?: string;
+}
